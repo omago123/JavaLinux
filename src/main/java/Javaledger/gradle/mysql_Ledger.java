151e069c2ebadd2c2684e218a1a9f23385c7775f
omago <omago123@naver.com>
2021-03-23 20:44:20 +0900

    Mysql

--- src/main/java/Javaledger/gradle/mysql_Ledger.java
+++ src/main/java/Javaledger/gradle/mysql_Ledger.java
@@ -1,5 +1,36 @@
 package Javaledger.gradle;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+class Mysql {
+
+
+    Connection conn = null;
+
+    //mysql 연결
+    Mysql() {
+        try {
+            conn = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/bookkeeping?autoReconnect=true&useSSL=false&serverTimezone=CTT",
+                    "root",
+                    "tiger"
+            );
+
+            //Statement stmt = conn.createStatement();
+            //ResultSet rs = stmt.executeQuery("select s.sname from s,p,sp where p.pname='e' && s.sno=sp.sno && sp.pno=p.pno");
+            //stmt.executeUpdate("drop database test1");
+
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+}
+
+
+
 public class mysql_Ledger {
 
 
--- src/main/java/mysql/Main.java
+++ src/main/java/mysql/Main.java
@@ -1,8 +1,6 @@
 package mysql;
 
 
-
-
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
